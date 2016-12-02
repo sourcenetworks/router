279bf2667169305ef7fd845924cf2716288f960d
Joe Farned <joefarned@gmail.com>
2016-12-02 13:47:16 -0500

    fix

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ import struct
 from payment import check_payment
 
 import requests
-from flask import Flask, server_
+from flask import Flask, server
 app = Flask(__name__)
 
 SERVER_ADDRESS = (HOST, PORT) = '', 9090
