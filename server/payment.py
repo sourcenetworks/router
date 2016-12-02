5b82a596520f9890c2aa7a115c6f192806f955a2
Joe Farned <joefarned@gmail.com>
2016-12-02 12:43:40 -0500

    fix

--- server/payment.py
+++ server/payment.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path="/templates/views/")
 registered = False
 
 def check_payment(request):
-    if register == False:
+    if registered == False:
         return app.send_static_file("logon.html")
 
     return
