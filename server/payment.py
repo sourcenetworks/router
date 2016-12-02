e150ec81da1381a02d5c4f79ca2281bf8e5c7c33
Joe Farned <joefarned@gmail.com>
2016-12-02 12:58:41 -0500

    fix

--- server/payment.py
+++ server/payment.py
@@ -1,13 +1,15 @@
-from flask import render_template
-
+from flask import Flask, render_template
+app = Flask(__name__)
 
 registered = False
 
+@app.route("/")
 def serve_logon():
     return render_template("logon.html")
 
 def check_payment(request):
     if registered == False:
+        app.run()
         serve_logon()
 
     return
