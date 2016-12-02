d06974f2eb1bfca589306fd3b6209a04aea7eb81
Joe Farned <joefarned@gmail.com>
2016-12-02 12:53:15 -0500

    fix

--- server/payment.py
+++ server/payment.py
@@ -1,16 +1,14 @@
-from flask import Flask, send_from_directory
+from flask import send_static_file
 
 app = Flask(__name__, static_url_path="/templates/views/")
 
 registered = False
 
-@app.route('/')
 def serve_logon():
-    return app.send_static_file("logon.html")
+    return send_static_file("logon.html")
 
 def check_payment(request):
     if registered == False:
-        app.run()
         serve_logon()
 
     return
