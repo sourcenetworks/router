79ee1d99c3dbc0fba757ddb2946f47b8b4b9e72a
Joe Farned <joefarned@gmail.com>
2016-12-02 14:08:49 -0500

    fix

--- server/server.py
+++ server/server.py
@@ -33,6 +33,7 @@ def free_proc(signum, frame):
 @app.route("/")
 def serve_logon():
     root_dir = os.path.dirname(os.getcwd())
+    print(os.path.join(root_dir, 'static', 'views'))
     return send_from_directory(os.path.join(root_dir, 'static', 'views'), "logon.html")
 
 """ The request object is a plain text representation of the
