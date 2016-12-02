2c4fb551aad1e0d0bda0b4c69c4386927e5a622c
Joe Farned <joefarned@gmail.com>
2016-12-02 15:24:02 -0500

    fix

--- server/server.py
+++ server/server.py
@@ -41,7 +41,7 @@ def handle_request(client_connection):
     print('Handling request')
     request = client_connection.recv(1024)
     response = b"""HTTP/1.1" 200 -
-                Content-Type: text/plain
+                Content-Type: html
         <!doctype html>
 
         <html lang="en">
