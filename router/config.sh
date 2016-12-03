92bace7eb8784cbb20a50899a99b362d55987672
Joe Farned <joefarned@gmail.com>
2016-12-03 11:15:02 -0500

    added config.sh

--- router/config.sh
+++ router/config.sh
@@ -1,7 +1,30 @@
-## This file will run the commands to set up an out of box rpi3 to run
-## source
+# Configure a fresh Raspbian install
+sudo apt-get update -y
+sudo apt-get install dnsmasq hostapd -y
 
-# Configure IP tables
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 9090
+# Configure hostapd (configuration for access point)
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+sudo cp interfaces /etc/network/interfaces
+sudo service dhcpcd restart
+sudo ifdown wlan0; sudo ifup wlan0
 
-# Configure NAT
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+sudo sed -i '/#DAEMON_CONF=""/c\DAEMON_CONF="/etc/hostapd/hostapd.conf"' \
+  /etc/default/hostapd
+
+# Configure dnsmasq (DHCP/DNS server)
+sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+
+# Configure iptables
+sudo iptables -F
+sudo iptables -i wlan0 -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -i wlan0 -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -i wlan0 -A INPUT -p udp --dport 53 -j ACCEPT
+sudo iptables -i wlan0 -A INPUT -p udp --dport 67:68 -j ACCEPT
+sudo iptables -i wlan0 -A INPUT -j DROP
+sudo sh -c "iptables-save > /etc/iptables.rules"
+
+# Run
+sudo service hostapd start
+sudo service dnsmasq start
+sudo reboot
--- server/configure.py
+++ server/configure.py
@@ -7,10 +7,12 @@ STATICFILES_DIRS = (
     os.path.join(os.path.dirname(__file__), "static", "views")
 )
 
-@app.route("/")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def serve_logon():
+    print("Hit")
     root_dir = os.path.dirname((os.path.realpath(__file__)))
     return send_from_directory(os.path.join(root_dir, 'static', 'views'), "logon.html")
 
 if __name__ == '__main__':
-    app.run(host='::', port=5000)
+    app.run(host='::', port=80)
